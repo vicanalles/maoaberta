47df88ea1418766d01d3bb20823ddc48f28b65e0
Vinicius Canalles <vicanalles@gmail.com>
2017-08-16 23:53:10 -0300

    Alteracoes de layout na tela de cadastro e login

--- app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/CadastroActivity.java
+++ app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/CadastroActivity.java
@@ -2,11 +2,14 @@ package com.maoaberta.vinicius.maoaberta.presentation.ui.activity;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import com.maoaberta.vinicius.maoaberta.R;
 import com.maoaberta.vinicius.maoaberta.presentation.ui.adapter.ViewPagerAdapter;
@@ -41,5 +44,12 @@ public class CadastroActivity extends AppCompatActivity {
         tabs.setTabTextColors(ColorStateList.valueOf(Color.WHITE));
         tabs.setSelectedTabIndicatorColor(Color.WHITE);
         tabs.setupWithViewPager(pager);
+
+        //Definição da fonte e da cor do texto das tabs
+        for(int i = 0; i < tabs.getTabCount(); i++){
+            TextView tv = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+            tv.setTypeface(Typeface.SANS_SERIF);
+            tabs.getTabAt(i).setCustomView(tv);
+        }
     }
 }
--- app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/LoginActivity.java
+++ app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.maoaberta.vinicius.maoaberta.R;
@@ -15,12 +16,16 @@ import com.maoaberta.vinicius.maoaberta.R;
 public class LoginActivity extends AppCompatActivity{
 
     TextView textViewAbrirCadastro;
+    EditText editTextEmail;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        editTextEmail = (EditText) findViewById(R.id.edit_text_login);
+        editTextPassword = (EditText) findViewById(R.id.edit_text_password);
         textViewAbrirCadastro = (TextView) findViewById(R.id.text_view_abrir_cadastro);
 
         textViewAbrirCadastro.setOnClickListener(new View.OnClickListener() {

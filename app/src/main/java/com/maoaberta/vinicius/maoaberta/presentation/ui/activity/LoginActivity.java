bfb7cb253896510d12fb356a45844e2b646c944d
Vinicius Canalles <Vinicius.Canalles@vistajet.com>
2017-08-18 15:06:38 -0300

    Alteracao no layout da tela de login

--- app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/LoginActivity.java
+++ app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/LoginActivity.java
@@ -1,11 +1,18 @@
 package com.maoaberta.vinicius.maoaberta.presentation.ui.activity;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,11 +22,12 @@ import com.maoaberta.vinicius.maoaberta.R;
  * Created by Vinicius on 12/08/2017.
  */
 
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends AppCompatActivity {
 
     TextView textViewAbrirCadastro;
     EditText editTextEmail;
     EditText editTextPassword;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,10 @@ public class LoginActivity extends AppCompatActivity{
         editTextEmail = (EditText) findViewById(R.id.edit_text_login);
         editTextPassword = (EditText) findViewById(R.id.edit_text_password);
         textViewAbrirCadastro = (TextView) findViewById(R.id.text_view_abrir_cadastro);
+        buttonLogin = (Button) findViewById(R.id.button_login_app);
+
+        editTextEmail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_account_circle, 0, 0, 0);
+        editTextPassword.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_lock_outline, 0, 0, 0);
 
         textViewAbrirCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +53,7 @@ public class LoginActivity extends AppCompatActivity{
         editTextEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
+                if (!hasFocus) {
                     hideKeyboard(v);
                 }
             }
@@ -50,15 +62,22 @@ public class LoginActivity extends AppCompatActivity{
         editTextPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
+                if (!hasFocus) {
                     hideKeyboard(v);
                 }
             }
         });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-    public void hideKeyboard(View view){
-        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }

1b657c18e20c003ad7f1d368c6eb0d29d410b953
Vinicius Canalles <vicanalles@gmail.com>
2017-08-20 12:49:43 -0300

    Criacao botao cadastro facebook

--- app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/fragment/TabCadastroCliente.java
+++ app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/fragment/TabCadastroCliente.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.maoaberta.vinicius.maoaberta.R;
 
@@ -14,10 +15,16 @@ import com.maoaberta.vinicius.maoaberta.R;
 
 public class TabCadastroCliente extends Fragment {
 
+    ImageView facebookIcon;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.tab_cadastro_cliente, container, false);
+
+        facebookIcon = (ImageView) v.findViewById(R.id.image_view_facebook_logo);
+        facebookIcon.setScaleType(ImageView.ScaleType.FIT_XY);
+
         return v;
     }
 }

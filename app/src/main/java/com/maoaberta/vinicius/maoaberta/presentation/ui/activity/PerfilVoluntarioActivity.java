a3426575fdf548d9f5d9121b799f31e769fb3993
Vinicius Canalles <vicanalles@gmail.com>
2017-10-16 08:43:17 -0200

    Criacao do metodo para selecionar foto de perfil do voluntario

--- app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/PerfilVoluntarioActivity.java
+++ app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/activity/PerfilVoluntarioActivity.java
@@ -3,6 +3,7 @@ package com.maoaberta.vinicius.maoaberta.presentation.ui.activity;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,8 +18,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -40,6 +44,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PerfilVoluntarioActivity extends AppCompatActivity {
 
+    private static final int SELECT_IMAGE = 1598;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     private UsuarioRepository usuarioRepository;
@@ -47,6 +52,8 @@ public class PerfilVoluntarioActivity extends AppCompatActivity {
 
     @BindView(R.id.relative_layout_image_logo_perfil_cliente)
     RelativeLayout relative_layout_image_logo_perfil_cliente;
+    @BindView(R.id.text_view_escolher_foto_perfil_cliente)
+    TextView text_view_escolher_foto;
     @BindView(R.id.image_view_logo_perfil_cliente)
     CircleImageView image_view_logo_perfil_cliente;
     @BindView(R.id.edit_text_nome_perfil_cliente)
@@ -120,6 +127,12 @@ public class PerfilVoluntarioActivity extends AppCompatActivity {
             });
         }
 
+        image_view_logo_perfil_cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirImagemDialog();
+            }
+        });
 
         botao_salvar_perfil_cliente.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -235,6 +248,28 @@ public class PerfilVoluntarioActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void abrirImagemDialog() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent.createChooser(intent, "Selecione uma imagem"), SELECT_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SELECT_IMAGE){
+            if(resultCode == RESULT_OK){
+                if(data != null){
+                    text_view_escolher_foto.setVisibility(View.GONE);
+                    Uri uri = data.getData();
+                    Glide.with(this).load(uri).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true)
+                            .into(image_view_logo_perfil_cliente);
+                }
+            }
+        }
     }
 
     @Override

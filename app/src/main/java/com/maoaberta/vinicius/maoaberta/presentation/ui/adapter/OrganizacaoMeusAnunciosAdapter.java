6a732c38d8277daa3f0fe839d1f3f05eeee965ba
Vinicius Canalles <vinicius@Viniciuss-iMac.monitora.int>
2017-10-25 17:47:27 -0200

    Creating adapter to show ads of an organization

---
+++ app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/adapter/OrganizacaoMeusAnunciosAdapter.java
@@ -0,0 +1,74 @@
+package com.maoaberta.vinicius.maoaberta.presentation.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.maoaberta.vinicius.maoaberta.R;
+import com.maoaberta.vinicius.maoaberta.domain.models.Anuncio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by vinicius on 25/10/17.
+ */
+
+public class OrganizacaoMeusAnunciosAdapter extends RecyclerView.Adapter<OrganizacaoMeusAnunciosAdapter.MyViewHolder>{
+
+    private Context context;
+    private List<Anuncio> anuncios;
+    private FirebaseUser currentUser;
+
+    public OrganizacaoMeusAnunciosAdapter(Context context){
+        this.context = context;
+        anuncios = new ArrayList<>();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    @Override
+    public OrganizacaoMeusAnunciosAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_meus_anuncios_organizacao, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Anuncio anuncio = anuncios.get(position);
+
+        holder.text_view_title_anuncio.setText(anuncio.getTitulo());
+        holder.text_view_data_inicio_anuncio.setText(anuncio.getDataInicio());
+        holder.text_view_data_termino_anuncio.setText(anuncio.getDataFim());
+    }
+
+    @Override
+    public int getItemCount() {
+        return anuncios.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.text_view_title_anuncio)
+        TextView text_view_title_anuncio;
+        @BindView(R.id.text_view_data_inicio_anuncio)
+        TextView text_view_data_inicio_anuncio;
+        @BindView(R.id.text_view_data_termino_anuncio)
+        TextView text_view_data_termino_anuncio;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+}
--- app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/fragment/FragmentMenuMeusAnunciosOrganizacao.java
+++ app/src/main/java/com/maoaberta/vinicius/maoaberta/presentation/ui/fragment/FragmentMenuMeusAnunciosOrganizacao.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +16,7 @@ import android.widget.Toast;
 
 import com.maoaberta.vinicius.maoaberta.R;
 import com.maoaberta.vinicius.maoaberta.presentation.ui.activity.CriacaoAnunciosActivity;
+import com.maoaberta.vinicius.maoaberta.presentation.ui.adapter.OrganizacaoMeusAnunciosAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,11 +32,23 @@ public class FragmentMenuMeusAnunciosOrganizacao extends Fragment {
     @BindView(R.id.recycler_ad_list)
     RecyclerView recyclerAdList;
 
+    private OrganizacaoMeusAnunciosAdapter organizacaoMeusAnunciosAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_menu_meus_anuncios_organizacao, container, false);
         ButterKnife.bind(this, view);
+
+        organizacaoMeusAnunciosAdapter = new OrganizacaoMeusAnunciosAdapter(getActivity());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerAdList.setLayoutManager(layoutManager);
+        recyclerAdList.setItemAnimator(new DefaultItemAnimator());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerAdList.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerAdList.addItemDecoration(dividerItemDecoration);
+        recyclerAdList.setAdapter(organizacaoMeusAnunciosAdapter);
+
+
 
         fab.setImageResource(R.drawable.ic_action_add);
         
